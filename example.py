7df279d549c4fb5a8915664c819eb7ef210543c4
Abraham Audu <abraham.audu@cantab.net>
2023-10-19 18:39:22 +0100

    Revising example file

--- example.py
+++ example.py
@@ -1,6 +1,6 @@
 import time
 
 # This is an example python script to test merge conflicts
-time.sleep(3)
-print(f"Hello World!, the time is {time.time()}")
-time.sleep(3)
+time.sleep(5)
+print(f"Hello World! The world is mine, the time is {time.time()}")
+time.sleep(5)
